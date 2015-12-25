9c728218ac74be5bcb4f30968b9dea6d3f542111
Piro / YUKI Hiroshi <piro.outsider.reflex@gmail.com>
2015-12-26 02:57:28 +0900

    Output result to a log file

--- handle_follow.sh
+++ handle_follow.sh
@@ -3,16 +3,15 @@
 work_dir="$(pwd)"
 tools_dir="$(cd "$(dirname "$0")" && pwd)"
 tweet_sh="$tools_dir/tweet.sh/tweet.sh"
+logfile="$work_dir/handle_follow.log"
 
 source "$tweet_sh"
 load_keys
 
-echo "FOLLOWED" 1>&2
 while read event
 do
-  echo "$event"
   screen_name="$(echo "$event" | jq -r .source.screen_name)"
-  echo "by $screen_name" 1>&2
-  echo "follow back $screen_name" 1>&2
-  "$tweet_sh" follow $screen_name
+  echo "Followed by $screen_name" >> "$logfile"
+  echo " => follow back $screen_name" >> "$logfile"
+  "$tweet_sh" follow $screen_name > /dev/null
 done
