bc76263e8be4f86dc5029fbe995c99fe99d5c243
Piro / YUKI Hiroshi <piro.outsider.reflex@gmail.com>
2015-12-26 04:46:42 +0900

    Run watch-mentions as a sub process

--- watch.sh
+++ watch.sh
@@ -6,8 +6,12 @@ tools_dir="$(cd "$(dirname "$0")" && pwd)"
 source "$tools_dir/tweet.sh/tweet.sh"
 load_keys
 
+trap 'kill $(jobs -p)' EXIT
+
 "$tools_dir/tweet.sh/tweet.sh" watch-mentions \
   -m "$tools_dir/handle_mention.sh" \
   -r "$tools_dir/handle_retweet.sh" \
   -q "$tools_dir/handle_quotation.sh" \
-  -f "$tools_dir/handle_follow.sh"
+  -f "$tools_dir/handle_follow.sh" &
+
+wait
