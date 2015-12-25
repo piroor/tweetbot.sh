52515d215ca5c3c85d00b220efc6f2faaa730132
Piro / YUKI Hiroshi <piro.outsider.reflex@gmail.com>
2015-12-26 05:39:25 +0900

    Fix syntax error

--- watch.sh
+++ watch.sh
@@ -15,7 +15,7 @@ then
   echo "Reading search queries from \"$queries_file\"" 1>&2
   queries="$(grep -v '^\s*#' "$queries_file" | \
                grep -v '^\s*$' | \
-               paste -s -d " OR ")
+               paste -s -d " OR ")"
   if [ "$queries" != '' ]
   then
     "$tools_dir/tweet.sh/tweet.sh" search \
