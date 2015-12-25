94ed5a2e10d505da25df621a74a076d015c7bf01
Piro / YUKI Hiroshi <piro.outsider.reflex@gmail.com>
2015-12-26 05:33:19 +0900

    Don't start multiple streaming

--- watch.sh
+++ watch.sh
@@ -9,28 +9,27 @@ load_keys
 trap 'jobs="$(jobs -p)"; [ "$jobs" = "" ] || kill $jobs' QUIT KILL TERM
 
 
-"$tools_dir/tweet.sh/tweet.sh" watch-mentions \
-  -m "$tools_dir/handle_mention.sh" \
-  -r "$tools_dir/handle_retweet.sh" \
-  -q "$tools_dir/handle_quotation.sh" \
-  -f "$tools_dir/handle_follow.sh" &
-
-
 queries_file="$work_dir/queries.txt"
 if [ -f "$queries_file" ]
 then
   echo "Reading search queries from \"$queries_file\"" 1>&2
+  queries=""
   grep -v '^\s*#' "$queries_file" | \
     grep -v '^\s*$' | \
     while read -r query
   do
-    "$tools_dir/tweet.sh/tweet.sh" search \
-      -q "$query" \
-      -h "$tools_dir/handle_search_result.sh" &
+    queries="$queries OR $query"
   done
+  queries="$(echo "$queries" | sed 's/^ OR //')"
+  "$tools_dir/tweet.sh/tweet.sh" search \
+    -q "$queries" \
+    -h "$tools_dir/handle_search_result.sh"
 else
   echo "There is no search query at \"$queries_file\"" 1>&2
+
+  "$tools_dir/tweet.sh/tweet.sh" watch-mentions \
+    -m "$tools_dir/handle_mention.sh" \
+    -r "$tools_dir/handle_retweet.sh" \
+    -q "$tools_dir/handle_quotation.sh" \
+    -f "$tools_dir/handle_follow.sh"
 fi
-
-
-wait
