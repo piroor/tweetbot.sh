50dc2e14613917835f8eb16498b66259304785fc
Piro / YUKI Hiroshi <piro.outsider.reflex@gmail.com>
2015-12-26 05:54:11 +0900

    Allow to specify extra tracking keywords

--- watch.sh
+++ watch.sh
@@ -10,24 +10,17 @@ trap 'jobs="$(jobs -p)"; [ "$jobs" = "" ] || kill $jobs' QUIT KILL TERM
 
 
 queries_file="$work_dir/queries.txt"
+queries=''
 if [ -f "$queries_file" ]
 then
   echo "Reading search queries from \"$queries_file\"" 1>&2
   queries="$(grep -v '^\s*#' "$queries_file" | \
                grep -v '^\s*$' | \
-               paste -s -d " OR ")"
-  if [ "$queries" != '' ]
-  then
-    "$tools_dir/tweet.sh/tweet.sh" search \
-      -q "$queries" \
-      -h "$tools_dir/handle_search_result.sh"
-    exit 0
-  fi
+               paste -s -d ",")"
 fi
 
-echo "There is no search query at \"$queries_file\"" 1>&2
-
 "$tools_dir/tweet.sh/tweet.sh" watch-mentions \
+  -k "$queries"
   -m "$tools_dir/handle_mention.sh" \
   -r "$tools_dir/handle_retweet.sh" \
   -q "$tools_dir/handle_quotation.sh" \
