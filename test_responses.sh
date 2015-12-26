95f0771ec36a3a267d9a98f379b2192af4f5fcda
Piro / YUKI Hiroshi <piro.outsider.reflex@gmail.com>
2015-12-27 00:32:10 +0900

    Add response tester script

---
+++ test_responses.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+work_dir="$(pwd)"
+tools_dir="$(cd "$(dirname "$0")" && pwd)"
+tweet_sh="$tools_dir/tweet.sh/tweet.sh"
+
+source "$tweet_sh"
+load_keys
+
+if [ "$TWEET_BASE_DIR" != '' ]
+then
+  TWEET_BASE_DIR="$(cd "$TWEET_BASE_DIR" && pwd)"
+else
+  TWEET_BASE_DIR="$work_dir"
+fi
+
+me="$("$tweet_sh" whoami)"
+
+cd "$TWEET_BASE_DIR"
+
+ls ./responses/* |
+  sort |
+  while read path
+do
+  # first, convert CR+LF => LF
+  nkf -Lu "$path" |
+    # ignore comment and blank lines
+    grep -v '^#|^\s*$' |
+    while read -r response
+    do
+      "$tweet_sh" post "@$me $response"
+    done
+done
