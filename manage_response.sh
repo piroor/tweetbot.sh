cbba36ce36b9644eb0c5041abe709917cac0d8ab
Piro / YUKI Hiroshi <piro.outsider.reflex@gmail.com>
2015-12-27 23:40:27 +0900

    Support deletion of responses

--- manage_response.sh
+++ manage_response.sh
@@ -58,6 +58,28 @@ then
 fi
 
 
+process_add_command() {
+  # if there is any file including the keyword in its name, then reuse it.
+  while read path
+  do
+    add_definition "$path" "$alias" "$response"
+    exit $?
+  done < <(find "$responses_dir" -type f -name "*${keyword}*")
+
+  # if there is any file including the keyword in its keyword definitions, then reuse it.
+  while read path
+  do
+    add_definition "$path" "$alias" "$response"
+    exit $?
+  done < <(egrep -r "^#\s*${keyword}\s*$" "$responses_dir" | cut -d ':' -f 1)
+
+  # otherwise, create new definition file.
+  path="$responses_dir/autoadd_${keyword}.txt"
+  echo "# $keyword" > "$path"
+  add_definition "$path" "$alias" "$response"
+  exit $?
+}
+
 add_definition() {
   local path=$1
   local alias=$2
@@ -110,26 +132,71 @@ add_definition() {
   fi
 }
 
+
+process_remove_command() {
+  # if there is any file including the keyword in its name, then reuse it.
+  while read path
+  do
+    remove_definition "$path" "$alias" "$response"
+    exit $?
+  done < <(find "$responses_dir" -type f -name "*${keyword}*")
+
+  # if there is any file including the keyword in its keyword definitions, then reuse it.
+  while read path
+  do
+    remove_definition "$path" "$alias" "$response"
+    exit $?
+  done < <(egrep -r "^#\s*${keyword}\s*$" "$responses_dir" | cut -d ':' -f 1)
+
+  exit 1
+}
+
+remove_definition() {
+  local path=$1
+  local alias=$2
+  local response=$3
+  local modified=0
+
+  log "Removing a response from $path..."
+
+  if [ "$alias" != '' ]
+  then
+    if egrep "^#\s*${alias}\s*$" "$path" > /dev/null
+    then
+      log "Removing alias \"$alias\" for \"$keyword\"..."
+      $esed -e "/^#${whitespaces}*${alias}${whitespaces}*$/d" -i "$path"
+      modified=1
+    fi
+  fi
+
+  if [ "$response" != '' ]
+  then
+    if egrep "^\s*${response}\s*$" "$path" > /dev/null
+    then
+      log "Removing response \"$response\"..."
+      $esed -e "/^${whitespaces}*${response}${whitespaces}*$/d" -i "$path"
+      modified=1
+    fi
+  fi
+
+  if [ $? = 0 ]
+  then
+    if [ $modified = 0 ]
+    then
+      log 'Nothing to be removed.'
+    else
+      log 'Successfully removed.'
+      "$tools_dir/generate_responder.sh"
+    fi
+    return 0
+  else
+    log 'Failed to remove.'
+    return 1
+  fi
+}
+
+
 case "$operation" in
-  add )
-# if there is any file including the keyword in its name, then reuse it.
-while read path
-do
-  add_definition "$path" "$alias" "$response"
-  exit $?
-done < <(find "$responses_dir" -type f -name "*${keyword}*")
-
-# if there is any file including the keyword in its keyword definitions, then reuse it.
-while read path
-do
-  add_definition "$path" "$alias" "$response"
-  exit $?
-done < <(egrep -r "^#\s*${keyword}\s*$" "$responses_dir" | cut -d ':' -f 1)
-
-# otherwise, create new definition file.
-path="$responses_dir/autoadd_${keyword}.txt"
-echo "# $keyword" > "$path"
-add_definition "$path" "$alias" "$response"
-exit $?
-;;
+  add ) process_add_command;;
+  del*|rem* ) process_remove_command;;
 esac
