3316cb4fd00e416406ceb26f1a9bdd81c82bab9c
YUKI Hiroshi <shimoda@clear-code.com>
2017-03-06 20:41:07 +0900

    Add uninstaller

---
+++ systemd/uninstall.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+names="$(cd /etc/systemd/system &&
+           find -type f -name "*_tweetbot.service" |
+           cut -d '/' -f 2 |
+           sed 's/_tweetbot.service$//')"
+
+if [ "$(echo "$names" | wc -l)" = "0" ]
+then
+  echo "Nothing to be uninstalled."
+  exit
+fi
+
+echo "$names" | nl
+read -p "Input the number of the bot to be uninstalled: " uninstall_index
+
+uninstall_name="$(echo "$names" | sed -n "${uninstall_index}p")"
+
+[ "$uninstall_name" = '' ] && exit
+
+read -p "Are you sure to uninstall the bot \"$uninstall_name\"?(y/N): " confirmation
+if echo "$confirmation" | egrep -i '^y' >/dev/null 2>&1
+then
+  systemctl stop "${uninstall_name}_tweetbot"
+  systemctl disable "${uninstall_name}_tweetbot"
+  rm "/etc/systemd/system/${uninstall_name}_tweetbot"
+  echo 'Done.'
+fi
+
