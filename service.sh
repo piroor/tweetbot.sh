068e69d9ea13e2c6e55839a475ed7a6df3c49dec
YUKI Hiroshi <shimoda@clear-code.com>
2017-03-06 16:35:04 +0900

    Add sample script to register as a service

---
+++ service.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+tools_dir="$(cd "$(dirname "$0")" && pwd)"
+pidfile="/tmp/.tweetbot.pidfile"
+
+if [ -f "$pidfile" ]
+then
+  pid="$(cat "$pidfile")"
+  kill "$pid"
+  rm "$pidfile"
+fi
+
+if [ "$1" != 'stop' ]
+then
+  #export TWEETBOT_DEBUG=1
+  "$tools_dir/tweetbot.sh/watch.sh" &
+  echo "$!" > "$pidfile"
+fi
