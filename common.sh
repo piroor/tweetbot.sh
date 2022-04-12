4bdc16c626d5c1463d30a42bdd6c044766b62936
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2022-04-12 23:27:29 +0900

    Accept only spaces

--- common.sh
+++ common.sh
@@ -801,6 +801,6 @@ then
            -e "s/[$whitespaces]*,+[$whitespaces]*/|/g" \
            -e 's/^\||\|$//g' \
            -e 's/"//g' \
-           -e "s/ /[$whitespaces]+/g" \
+           -e "s/ /[ 　]+/g" \
   )"
 fi
